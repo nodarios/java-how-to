7c9ab96bd4fd60ba2f2cf4d351718ba8a49bd6b6
Nodar Sakvarelidze <nsakvarelidze12@gmail.com>
2020-08-27 19:34:18 +0400

    minor changes

---
+++ src/main/java/com/cmp/javahowto/constants/Day.java
@@ -0,0 +1,21 @@
+package com.cmp.javahowto.constants;
+
+public enum Day {
+    MONDAY(1),
+    TUESDAY(2),
+    WEDNESDAY(3),
+    THURSDAY(4),
+    FRIDAY(5),
+    SATURDAY(6),
+    SUNDAY(7);
+
+    private final int dayNumber;
+
+    Day(int dayNumber) {
+        this.dayNumber = dayNumber;
+    }
+
+    int getDayNumber() {
+        return dayNumber;
+    }
+}
---
+++ src/main/java/com/cmp/javahowto/constants/Test.java
@@ -0,0 +1,11 @@
+package com.cmp.javahowto.constants;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Day day = Day.SATURDAY;
+        System.out.println(day);
+        System.out.println(day.getDayNumber());
+    }
+
+}
