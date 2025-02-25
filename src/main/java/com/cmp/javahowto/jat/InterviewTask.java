893bc8e3535ddde24815fdb3d29e456d72d6773d
Nodar Sakvarelidze <nodarios@gmail.com>
2025-02-25 14:53:44 +0400

    minor change

--- src/main/java/com/cmp/javahowto/jat/InterviewTask.java
+++ src/main/java/com/cmp/javahowto/jat/InterviewTask.java
@@ -1,20 +1,38 @@
 package com.cmp.javahowto.jat;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class InterviewTask {
 
     public static void main(String[] args) {
-
         //int[] transactions = new int[]{1, 1, 3, 1, 5};
         //System.out.println(findIndexWhereElementsSumBeforeAndAfterAreEqual(transactions));
 
         //System.out.println(removeConsecutiveDuplicates("abccccda"));
 
-        int[] arr = new int[]{1, 2, 3, 4, 5};
-        reverseArray(arr);
-        System.out.println(Arrays.toString(arr));
+        //int[] arr = new int[]{1, 2, 3, 4, 5};
+        //reverseArray(arr);
+        //System.out.println(Arrays.toString(arr));
 
+        // Given an array of integers, write a function to return the maximum sum of contiguous sub arrays.
+        // Contiguous sub arrays are: "-2", "-2, 1", "-2, 1, -3" and so on.
+        int[] inputArray = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        OptionalInt maxSum = findMaxSubArraySum(inputArray);
+        if (maxSum.isPresent()) {
+            System.out.println("Maximum sum of a contiguous sub arrays is " + maxSum.getAsInt());
+        }
+    }
+
+    private static OptionalInt findMaxSubArraySum(int[] inputArray) {
+        if (inputArray == null || inputArray.length == 0) {
+            return OptionalInt.empty();
+        }
+        List<Integer> sumList = new ArrayList<>();
+        sumList.add(inputArray[0]);
+        for (int i = 1; i < inputArray.length; i++) {
+            sumList.add(sumList.get(i - 1) + inputArray[i]);
+        }
+        return sumList.stream().mapToInt(Integer::intValue).max();
     }
 
     private static void reverseArray(int[] arr) {
