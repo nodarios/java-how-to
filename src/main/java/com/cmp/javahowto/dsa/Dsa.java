b00cc59898b96414582a55afb09b85e59f27cdeb
Nodar Sakvarelidze <nodarios@gmail.com>
2025-02-28 18:10:47 +0400

    dsa

---
+++ src/main/java/com/cmp/javahowto/dsa/Dsa.java
@@ -0,0 +1,38 @@
+package com.cmp.javahowto.dsa;
+
+import java.util.*;
+
+public class Dsa {
+
+    public static void main(String[] args) {
+        insertionSort();
+    }
+
+    public static void insertionSort() {
+        // first element is considered to be sorted
+        // start with next element and compare it with previous elements and insert where appropriate
+        // no swap, only shifts
+        int[] arr = new int[]{6, 4, 2, 8, 9, 7};
+        for (int i = 1; i < arr.length; i++) {
+            int tempValue = arr[i];
+            int tempIndex = i;
+            for (int j = i - 1; j >= 0 && tempValue < arr[j]; j--) {
+                arr[tempIndex] = arr[j];
+                tempIndex = j;
+            }
+            if (tempIndex != i) {
+                arr[tempIndex] = tempValue;
+            }
+            //int j = i - 1;
+            //while (j >= 0 && tempValue < arr[j]) {
+            //    arr[j + 1] = arr[j];
+            //    j--;
+            //}
+            //if (i - 1 != j) {
+            //    arr[j + 1] = tempValue;
+            //}
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
