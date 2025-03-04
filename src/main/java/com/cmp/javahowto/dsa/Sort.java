e74ef13a36c961e47dccb2659e31a25a6b969036
Nodar Sakvarelidze <nodarios@gmail.com>
2025-03-04 21:14:26 +0400

    dsa

--- src/main/java/com/cmp/javahowto/dsa/Sort.java
+++ src/main/java/com/cmp/javahowto/dsa/Sort.java
@@ -17,9 +17,79 @@ public class Sort {
         //insertionSort(arr);
         //System.out.println(Arrays.toString(arr));
 
-        int[] arr = new int[]{5, 2, 6, 1, 4, 3};
-        quickSort(arr, 0, arr.length - 1);
+        //int[] arr = new int[]{5, 2, 6, 1, 4, 3};
+        //quickSort(arr, 0, arr.length - 1);
+        //System.out.println(Arrays.toString(arr));
+
+        int[] arr = new int[]{4, 2, 1, 6, 5, 3};
+        mergeSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
+    }
+
+    // takes O(n log n)
+    public static void mergeSort(int[] arr, int left, int right) {
+        // 4, 2, 1, 6, 5, 3
+        // 4, 2, 1 | 6, 5, 3
+        // 4, 2 | 1 | 6, 5 | 3
+        // 4 | 2 | 1 | 6 | 5 | 3
+        // 2, 4 | 1 | 5, 6 | 3
+        // 1, 2, 4 | 3, 5, 6
+        // 1, 2, 3, 4, 5, 6
+        if (left < right) {
+            //int mid = (left + right)/2;
+            int mid = left + (right - left) / 2;
+
+            // Recursively divide the array into left and right halves
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int leftSize = mid + 1 - left;  // Size of left half
+        int rightSize = right - mid;      // Size of right half
+
+        // Temporary arrays
+        int[] leftArr = new int[leftSize];
+        int[] rightArr = new int[rightSize];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < leftSize; i++) {
+            leftArr[i] = arr[left + i];
+        }
+        for (int i = 0; i < rightSize; i++) {
+            rightArr[i] = arr[mid + 1 + i];
+        }
+
+        // Merge the temp arrays back into arr[] in sorted way
+        int i = 0, j = 0, k  = left;
+
+        while (i < leftSize && j < rightSize) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements (if any)
+        while (i < leftSize) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < rightSize) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
     }
 
     // takes O(n log n)
