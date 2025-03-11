fda29b2cfb7d42daa09231aeb7cb22b259575f17
Nodar Sakvarelidze <nodarios@gmail.com>
2025-03-11 13:57:06 +0400

    dsa

--- src/main/java/com/cmp/javahowto/dsa/structure/MyCircularQueue.java
+++ src/main/java/com/cmp/javahowto/dsa/structure/MyCircularQueue.java
@@ -5,32 +5,30 @@ public class MyCircularQueue<E> {
     private Object[] queue = new Object[3];
     private int tail = 0;
     private int head = 0;
-    private boolean isEmpty = true;
+    private int size = 0;
 
     public boolean offer(E element) {
-        if (tail != head || isEmpty) {
+        if (tail != head || size == 0) {
             queue[tail] = element;
             tail = (tail + 1) % queue.length;
-            isEmpty = false;
+            size++;
             return true;
         }
         return false;
     }
 
     public E peek() {
-        if (!isEmpty) {
+        if (size > 0) {
             return (E) queue[head];
         }
         return null;
     }
 
     public E poll() {
-        if (!isEmpty) {
+        if (size > 0) {
             E element = (E) queue[head];
             head = (head + 1) % queue.length;
-            if (head == tail) {
-                isEmpty = true;
-            }
+            size--;
             return element;
         }
         return null;
