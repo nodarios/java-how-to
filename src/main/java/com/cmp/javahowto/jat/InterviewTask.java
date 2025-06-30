cb0f9dab48733ae5d2c7ec5b0d31ce65e99bd56b
Nodar Sakvarelidze <nodarios@gmail.com>
2025-06-30 23:06:52 +0400

    updated tests

--- src/main/java/com/cmp/javahowto/jat/InterviewTask.java
+++ src/main/java/com/cmp/javahowto/jat/InterviewTask.java
@@ -1,13 +1,17 @@
 package com.cmp.javahowto.jat;
 
-import java.util.*;
-import java.util.stream.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
 
 public class InterviewTask {
 
     public static void main(String[] args) {
-        //int[] transactions = new int[]{1, 1, 3, 1, 5};
-        //System.out.println(findIndexWhereElementsSumBeforeAndAfterAreEqual(transactions));
+        //int[] inputArray = new int[]{1, 1, 3, 1, 5};
+        //System.out.println(findIndexWhereElementsSumBeforeAndAfterAreEqual(inputArray));
 
         //System.out.println(removeConsecutiveDuplicates("abccccda"));
 
@@ -26,8 +30,8 @@ public class InterviewTask {
         // Assume you have an array [1,2,3,4,5], you need to return subArrays
         // e.g. [1], [1,2], [1,2,3], [1,2,3,4], [1,2,3,4,5], [2], [2,3], [2,3,4], [2,3,4,5], [3]...
         //int[] inputArray = new int[]{1, 2, 3, 4, 5};
-        //List<List<Integer>> result = getSubArrays(inputArray);
-        //for (List<Integer> subArray : result) {
+        //List<List<Integer>> subArrays = getSubArrays(inputArray);
+        //for (List<Integer> subArray : subArrays) {
         //    System.out.println(subArray);
         //}
 
@@ -41,6 +45,13 @@ public class InterviewTask {
 
         streamGrouping();
     }
+
+    // {} -- Valid expression
+    // ({}[]) -- Valid
+    // {[}] -- Invalid
+    // ()[]{} -- Valid
+    // {[()]} -- Valid
+    // {[(])} -- Invalid
 
     private static void streamGrouping() {
         record Person(int id, String name, int age, String city) {
@@ -61,13 +72,6 @@ public class InterviewTask {
         System.out.println(result);
     }
 
-    // {} -- Valid expression
-    // ({}[]) -- Valid
-    // {[}] -- Invalid
-    // ()[]{} -- Valid
-    // {[()]} -- Valid
-    // {[(])} -- Invalid
-
     private static int findSmallestElementIndexInRotatedSortedArray(int[] inputArray) {
         for (int i = 1; i < inputArray.length; i++) {
             if (inputArray[i - 1] > inputArray[i]) {
@@ -78,19 +82,17 @@ public class InterviewTask {
     }
 
     private static List<List<Integer>> getSubArrays(int[] inputArray) {
-        List<List<Integer>> list = new ArrayList<>();
+        List<List<Integer>> subArrays = new ArrayList<>();
         for (int i = 0; i < inputArray.length; i++) {
             for (int j = i; j < inputArray.length; j++) {
-                list.add(getSubArray(inputArray, i, j + 1));
+                List<Integer> subArray = new ArrayList<>();
+                for (int k = i; k <= j; k++) {
+                    subArray.add(inputArray[k]);
+                }
+                subArrays.add(subArray);
             }
         }
-        return list;
-    }
-
-    private static List<Integer> getSubArray(int[] inputArray, int leftIndex, int rightIndex) {
-        return Arrays.stream(Arrays.copyOfRange(inputArray, leftIndex, rightIndex))
-                .boxed()
-                .toList();
+        return subArrays;
     }
 
     private static OptionalInt findMaxSubArraySum(int[] inputArray) {
@@ -115,43 +117,30 @@ public class InterviewTask {
             startIndex++;
             endIndex--;
         }
-
     }
 
     private static String removeConsecutiveDuplicates(String s) {
-        var chars = s.toCharArray();
-        char currentChar;
-        char previousChar = 0;
-        var sb = new StringBuilder();
-        for (int i = 0; i < chars.length; i++) {
-            currentChar = chars[i];
-            if (i == 0) {
-                sb.append(currentChar);
-                previousChar = currentChar;
-                continue;
+        String[] arr = s.split("");
+        StringBuilder sb = new StringBuilder();
+        sb.append(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (!arr[i].equals(arr[i - 1])) {
+                sb.append(arr[i]);
             }
-            if (currentChar != previousChar) {
-                sb.append(currentChar);
-            }
-            previousChar = currentChar;
         }
         return sb.toString();
     }
 
-    public static int findIndexWhereElementsSumBeforeAndAfterAreEqual(int[] transactions) {
-
-        for (int i = 0; i < transactions.length; i++) {
-            if (i > 0 && i < transactions.length - 1) {
-                int[] beforeArray = Arrays.copyOfRange(transactions, 0, i);
-                int[] afterArray = Arrays.copyOfRange(transactions, i + 1, transactions.length);
-                var beforeSum = Arrays.stream(beforeArray).sum();
-                var afterSum = Arrays.stream(afterArray).sum();
-                if (beforeSum == afterSum) {
-                    return i;
-                }
+    public static int findIndexWhereElementsSumBeforeAndAfterAreEqual(int[] inputArray) {
+        for (int i = 1; i < inputArray.length - 1; i++) {
+            int[] beforeArray = Arrays.copyOfRange(inputArray, 0, i);
+            int[] afterArray = Arrays.copyOfRange(inputArray, i + 1, inputArray.length);
+            var beforeSum = Arrays.stream(beforeArray).sum();
+            var afterSum = Arrays.stream(afterArray).sum();
+            if (beforeSum == afterSum) {
+                return i;
             }
         }
-
         return -1;
     }
 
